4a9b482218de180336b96261b34e2df4b311d7dc
Houssem Dellai <houssem.dellai@live.com>
2021-06-04 13:41:02 +0200

    repo creation with script files

--- dynamic-file-share/commands.sh
+++ dynamic-file-share/commands.sh
@@ -1,18 +1,19 @@
 # src: https://docs.microsoft.com/en-us/azure/aks/azure-files-dynamic-pv
 
+# Deploy the Storage Class and PVC to create the Storage Account & File Share
 kubectl apply -f azure-file-sc.yaml
 kubectl apply -f azure-file-pvc.yaml
 
-# Once completed, the Azure File Share will be created (view it in the portal). 
+# The Azure File Share will be created (view it in the portal). 
 
-# A Kubernetes secret is also created that includes connection information 
-# and credentials.
+# A Kubernetes secret is also created that includes Storage Account Key.
 kubectl get secrets
 kubectl get secret <azure-storage-account-name-secret> -o yaml
 
-kubectl apply -f azure-pvc-files.yaml
+# Deploy sample Nginx Pod and attach it to the File Share
+kubectl apply -f app-pod.yaml
 
-# test access to File Share from the Pod:
+# Test access to File Share from the Pod:
 kubectl exec pod/nginx -it -- /bin/sh
 # From inside the Pod shell:
 # ls /mnt/azure
@@ -20,8 +21,5 @@ kubectl exec pod/nginx -it -- /bin/sh
 # ls /mnt/azure
 # cat /mnt/azure/myfile.txt
 
-# You can also check the File Share in the newly created Storage Account
-# from the Azure Portal.
-
-# options for mounting File Share: 
+# More options for mounting File Share: 
 # https://github.com/kubernetes-sigs/azurefile-csi-driver/blob/master/docs/driver-parameters.md#dynamic-provision
